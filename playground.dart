268a66cf754fc2fe1c7a70a5b72cfb07dc51ce66
sabiha <105931254+ThisisNirmalJaiswal@users.noreply.github.com>
2023-04-01 20:06:53 +0530

    dart-learning

---
+++ playground.dart
@@ -0,0 +1,113 @@
+// main(){
+//     print("Hello World");
+// }
+
+/*
+Strongly typed language:- The type of a variable is known at compile time.
+for Example:- c++, Java, Swift.
+
+Dynamic Typed language:- The type of variable is known at run time.
+for example:- Ruby, Python, Javascript.
+*/
+
+// data types in dart
+
+// main() {
+//   /*
+//         int
+//         double
+//         String
+//         bool
+//         Dynamic
+//     */
+
+//   int amount1 = 100;
+//   var amount2 = 200;
+//   print('Amount1: $amount1 | Amount2: $amount2 \n');
+
+//   double dAmount1 = 110.11;
+//   var dAmount2 = 200.22;
+//   print('dAmount1: $dAmount1 | dAmount2: $dAmount2 \n');
+
+//   String name1 = "Nirmal";
+//   var name2 = "Jasval";
+//   print('My name is: $name1 $name2 \n');
+
+//   bool isTrue1 = true;
+//   var isTrue2 = false;
+//   print('isTrue1: $isTrue1 | isTrue2: $isTrue2 \n');
+
+//   dynamic weakVariable1 = 100;
+//   print('weakVariable 1: $weakVariable1 \n');
+
+//   weakVariable1 = 'dart tutorial';
+//   print('weakVariable 1: $weakVariable1 \n');
+// }
+
+// string
+import 'dart:math';
+
+main() {
+  // var s1 = 'single quotes work well for string literals.';
+  // var s2 = "double quotes work just as well.";
+  // var s3 = 'It\'s easy to escape the string delimiter.';
+  // var s4 = "It's even easier to use the other delimiter.";
+
+  // print(s1);
+  // print(s2);
+  // print(s3);
+  // print(s4);
+
+  // // RAW String
+  // var s = r'In a raw string, not even \n gets special treatment.';
+  // print(s);
+
+  // var age = 35;
+  // var str = 'My age is: $age';
+  // print(str);
+
+  // var s1 = '''
+  // you can create
+  // multi-line strings like this one.
+  // ''';
+
+  // var s2 = """
+  // This is also a
+  // multi-line string. """;
+
+  // print(s1);
+  // print(s2);
+
+  // String to int
+  // var one = int.parse('1');
+  // assert(one == 1);
+
+  // // String to double
+  // var onePointOne = double.parse('1.1');
+  // assert(onePointOne == 1.1);
+
+  // int to String
+  // String oneAsString = 1.toString();
+  // assert(oneAsString == '1');
+
+  // // double to String
+  // String piAsString = 3.14159.toStringAsFixed(2);
+  // assert(piAsString == 3.14);
+
+// // const keyword
+//   const aConstNum = 0; // int constant
+//   const aConstBool = true; // bool constant
+//   const aConstString = 'A const String'; // String Constant
+
+//   print(aConstNum);
+//   print(aConstBool);
+//   print(aConstString);
+
+//   print(aConstNum.runtimeType);
+//   print(aConstBool.runtimeType);
+//   print(aConstString.runtimeType);
+
+  // int num;
+
+  // print(num);
+}
