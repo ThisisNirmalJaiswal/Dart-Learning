a13cbe428fe8fa7f2b1b1fb31ee7c332843ac8b1
sabiha <105931254+ThisisNirmalJaiswal@users.noreply.github.com>
2023-04-01 20:16:07 +0530

    operator

--- operators.dart
+++ operators.dart
@@ -1,3 +1,33 @@
 main() {
+  int num = 10 + 22;
+  num = num - 2;
 
+  print(num);
+
+  // Relational ==, !=, >=, <=
+  if (num == 0) {
+    print('zero');
+  }
+
+  num = 100;
+  num *= 2; // num = num * 2;
+  print(num);
+
+// Unary Operator
+  ++num;
+  num++;
+  num += 1;
+  num -= 1;
+  print(num);
+
+  // logical && and logical ||
+
+  if (num > 200 && num < 203) {
+    print('200 to 202');
+  }
+
+  // != is not equal
+  if (num != 100) {
+    print('num is not equal to 100');
+  }
 }
