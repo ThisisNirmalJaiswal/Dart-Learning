b196fa8b8cb4aeb163c437266b91b4c4e88304a4
sabiha <105931254+ThisisNirmalJaiswal@users.noreply.github.com>
2023-04-01 20:59:38 +0530

    Collections(map,set,list)

---
+++ collection.dart
@@ -0,0 +1,49 @@
+// Collection
+// List, Set, Map
+
+// main() {
+//   // // List :- it is an ordered collection of values
+
+//   // List<String> names = ['jack', 'jill'];
+
+//   // var names2 = [...names];
+
+//   // names[1] = 'mark';
+
+//   // for (var n in names2) {
+//   //   print(n);
+//   // }
+// }
+
+// main() {
+//   // Set :- it is an unordered collection of unique items;
+//   // var halogens = {'fluorine', 'chlorine', 'fluorine'};
+
+//   // for (var x in halogens) {
+//   //   print(x);
+//   // }
+
+//   var helogens = <String>{}; // _Set<String>
+//   Set names = <String>{}; // _Set<String>
+//   print(helogens.runtimeType);
+//   print(names.runtimeType);
+
+// }
+
+main() {
+  // Map:- it is a collection of key-value pair of items
+
+  // var gifts = {
+  //   // key: value
+  //   1: 'partridge',
+  //   2: 'turtledoves',
+  //   3: 'goldenRings'
+  // };
+
+  // var gifts = Map();
+  // gifts['first'] = "mango";
+
+  var gifts = {'first': 'mango', 'second': "Jack Fruit"};
+
+  print(gifts);
+}
